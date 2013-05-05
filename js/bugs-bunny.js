fa5bf83dee66cb1afb512f1a40eabf523c1120fa
Maximilian Sohrt <max@sohrt.de>
2013-05-05 20:09:51 +0200

    Just some further improvements. Nothing in specific

--- js/bugs-bunny.js
+++ js/bugs-bunny.js
@@ -7,29 +7,80 @@
  *
  * http://help.dottoro.com/ljwupxoh.php
  */
+
+navigator.sayswho= (function(){
+  var N= navigator.appName, ua= navigator.userAgent, tem;
+  var M= ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
+  if(M && (tem= ua.match(/version\/([\.\d]+)/i))!= null) M[2]= tem[1];
+  M= M? [M[1], M[2]]: [N, navigator.appVersion,'-?'];
+  return M;
+})();
+
 var bugsBunny  = (function(){
 
   var
     container,
-    browserVersion
+    viewportHeight,
+    viewportWidth,
+    viewportHeight = $(window).height(),
+    viewportWidth = $(window).width(),
+    offsetVertical = 350,
+    offsetHorizontal = 350,
+    browser,
+    browserOS,
+    clientIP
     ;
 
-  function getBrowserInfo() {
-    browserVersion = navigator.userAgent;
+  /**
+   * Defines inital position of window
+   * @param $elem The element to be positioned
+   */
+  function placeWindow($elem) {
+    $elem.css("top", viewportHeight - offsetVertical);
+    $elem.css("left", viewportWidth  - offsetHorizontal);
+  };
+
+  function getBrowser() {
+    browser = navigator.sayswho;
+//    browserType = BrowserDetect.browser;
+  };
+
+  function getClientIP() {
+    $.getJSON('http://api.hostip.info/get_json.php', null,
+      function(data){
+        clientIP = data.ip;
+        $("#handle").after("<b>Client IP: </b>" + clientIP + "<br>");
+      });
+  };
+
+  function getClientOS() {
+    browserOS = navigator.platform;
+//    browserOS = navigator.userAgent;
+//    browserOS = BrowserDetect.OS;
   };
 
   function writeInfo() {
-    console.log(browserVersion);
-    container.append(browserVersion);
+//    container.append("<b>Client IP:</b>" + clientIP + "<br>");
+    container.append("<b>Browser: </b>" + browser + "<br>");
+    container.append("<b>OS: </b>" + browserOS + "<br>");
   };
 
   function init() {
     container = $("#bugsBunny");
-    getBrowserInfo();
+    placeWindow(container);
+    getBrowser();
+    getClientIP();
+    getClientOS();
     writeInfo();
-    console.log(container);
-    container.draggable({handle: "p" });
-    container.resizable({});
+
+    container.draggable({
+      handle: "p"
+    });
+
+    container.resizable({
+      minHeight: 50,
+      minWidth: 50
+    });
   };
 
   return {
@@ -38,6 +89,6 @@ var bugsBunny  = (function(){
 
 })();
 
-$(document).ready(function(){
+$(window).load(function(){
   bugsBunny.init();
 });
